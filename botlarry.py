cea8873c3de4141bd3ac218841a195cbaf1af82b
notlarry <notlarry@hotmail.com>
2021-05-09 15:38:57 +0100

    kind of starting over

---
+++ botlarry.py
@@ -0,0 +1,8 @@
+#!/home/notlarry/venvlarry/bin/python
+"""
+
+  botlarry.py initial entry point for botlarry.  At this time it is assumed this will be the code started by systemctl at startup and will continue to run.
+
+  ### build out a unit file so that this always runs
+
+"""
