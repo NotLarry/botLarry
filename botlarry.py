f81d9d1b760d99084193d93bac76e66201fcd3f4
NotLarry <botlarry@botlarry.r00tw4rz>
2025-03-11 18:47:11 -0500

    code for setting filename

--- botlarry.py
+++ botlarry.py
@@ -1,4 +1,4 @@
-#!/home/notlarry/venvlarry/bin/python
+#!/home/botlarry/phonehome/bin/python3
 """
 
   botlarry.py initial entry point for botlarry.  At this time it is assumed this will be the code started by systemctl at startup and will continue to run.
@@ -6,3 +6,10 @@
   ### build out a unit file so that this always runs
 
 """
+
+from datetime import datetime
+
+# datetime.today().strftime('%Y%m%d%H%M%s')
+filename = datetime.today().strftime('%Y%m%d%H%M%S')+'.mp3'
+print(filename)
+
