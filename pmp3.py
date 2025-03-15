d6aa665d95cba53153c49f16ddf4b12c10249aca
NotLarry <botlarry@botlarry.r00tw4rz>
2025-03-14 19:08:09 -0500

    I don't know, something I found in this directory

---
+++ pmp3.py
@@ -0,0 +1,14 @@
+#!/home/botlarry/phonehome/bin/python3
+"""
+  pmp3.py
+
+"""
+# import required module
+import vlc
+
+# for playing note.mp3 file
+
+p = vlc.MediaPlayer("file:///home/botlarry/.playground/git/botlarry/media/YYZ.mp3")
+p.play()
+
+
