da27a2bdd39456d77b9fa264ad016434bab527a5
NotLarry <notlarry@hotmail.com>
2025-03-23 14:54:32 -0500

    update comments on keypad.py

--- bin/keypad.py
+++ bin/keypad.py
@@ -1,3 +1,20 @@
+"""
+keypad.py may be the heart of botlarry.  It will need to perform a signifigant amount of the process.
+
+1) send dailtone to earpiece. (timeout 20 seconds)
+2) when a key is pressed send a tone specific to that key to the earpiece, record the digit
+3) When 10 digits have been processed
+4) Check if first digit is '1', if so call audio.py with local calls only, then return to dialtone
+5) check the first 3 digits for area code.
+  a) for 666 area codes look up the 10 digit # in database
+    1) if it has an entry call audio.py with coresponding mp3 file
+    2) if it does not have an entry call audio.py with 'number is not assigned' mp3
+    3) return to hook.py
+
+"""
+
+# the following is keypad code for the nodemcu.  This will need to be replaced.
+
 import adafruit_matrixkeypad
 from digitalio import DigitalInOut
 import board
