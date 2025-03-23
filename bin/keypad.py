fc8418a099f65b033f2d817a95d0cf505d35e710
NotLarry <notlarry@hotmail.com>
2025-03-23 15:37:52 -0500

    changed keypad.py for raspberry pi instead of nodemcu

--- bin/keypad.py
+++ bin/keypad.py
@@ -12,29 +12,110 @@ keypad.py may be the heart of botlarry.  It will need to perform a signifigant a
     3) return to hook.py
 
 """
-from matrix_keypad import RPi_GPIO
+
+# #####################################################
+# Python Library for 3x4 matrix keypad using
+# 7 of the avialable GPIO pins on the Raspberry Pi. 
+# 
+# This could easily be expanded to handle a 4x4 but I 
+# don't have one for testing. The KEYPAD constant 
+# would need to be updated. Also the setting/checking
+# of the colVal part would need to be expanded to 
+# handle the extra column.
+# 
+# Written by Chris Crumpacker
+# May 2013
+#
+# main structure is adapted from Bandono's
+# matrixQPI which is wiringPi based.
+# https://github.com/bandono/matrixQPi?source=cc
+# #####################################################
+
+import RPi.GPIO as GPIO
+
+class keypad():
+    # CONSTANTS   
+    KEYPAD = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    ["*",0,"#"]
+    ]
+    
+    ROW         = [26,19,13,6]
+    COLUMN      = [21,20,16]
+    
+    def __init__(self):
+        GPIO.setmode(GPIO.BCM)
+    
+    def getKey(self):
+        
+        # Set all columns as output low
+        for j in range(len(self.COLUMN)):
+            GPIO.setup(self.COLUMN[j], GPIO.OUT)
+            GPIO.output(self.COLUMN[j], GPIO.LOW)
+        
+        # Set all rows as input
+        for i in range(len(self.ROW)):
+            GPIO.setup(self.ROW[i], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        
+        # Scan rows for pushed key/button
+        # A valid key press should set "rowVal"  between 0 and 3.
+        rowVal = -1
+        for i in range(len(self.ROW)):
+            tmpRead = GPIO.input(self.ROW[i])
+            if tmpRead == 0:
+                rowVal = i
+                
+        # if rowVal is not 0 thru 3 then no button was pressed and we can exit
+        if rowVal <0 data-blogger-escaped-or="" data-blogger-escaped-rowval="">3:
+            self.exit()
+            return
+        
+        # Convert columns to input
+        for j in range(len(self.COLUMN)):
+                GPIO.setup(self.COLUMN[j], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        
+        # Switch the i-th row found from scan to output
+        GPIO.setup(self.ROW[rowVal], GPIO.OUT)
+        GPIO.output(self.ROW[rowVal], GPIO.HIGH)
+
+        # Scan columns for still-pushed key/button
+        # A valid key press should set "colVal"  between 0 and 2.
+        colVal = -1
+        for j in range(len(self.COLUMN)):
+            tmpRead = GPIO.input(self.COLUMN[j])
+            if tmpRead == 1:
+                colVal=j
+                
+        # if colVal is not 0 thru 2 then no button was pressed and we can exit
+        if colVal <0 data-blogger-escaped-colval="" data-blogger-escaped-or="">2:
+            self.exit()
+            return
+
+        # Return the value of the key pressed
+        self.exit()
+        return self.KEYPAD[rowVal][colVal]
+        
+    def exit(self):
+        # Reinitialize all rows and columns as input at exit
+        for i in range(len(self.ROW)):
+                GPIO.setup(self.ROW[i], GPIO.IN, pull_up_down=GPIO.PUD_UP) 
+        for j in range(len(self.COLUMN)):
+                GPIO.setup(self.COLUMN[j], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        
+if __name__ == '__main__':
+    # Initialize the keypad class
+    kp = keypad()
+    
+    # Loop while waiting for a keypress
+    digit = None
+    while digit == None:
+        digit = kp.getKey()
+    
+    # Print the result
+    print digit  
 
 
 
 
-# the following is keypad code for the nodemcu.  This will need to be replaced.
-
-import adafruit_matrixkeypad
-from digitalio import DigitalInOut
-import board
-
-# botlarry's 3x4 matrix keypad
-cols = [DigitalInOut(x) for x in (board.C2, board.C0, board.C4)]
-rows = [DigitalInOut(x) for x in (board.C1, board.C6, board.C5, board.C3)]
-keys = ((1, 2, 3),
-        (4, 5, 6),
-        (7, 8, 9),
-        ('*', 0, '#'))
-
-keypad = adafruit_matrixkeypad.Matrix_Keypad(rows, cols, keys)
-
-while True:
-    keys = keypad.pressed_keys
-    if keys:
-        print("Pressed: ", keys)
-    time.sleep(0.1)
